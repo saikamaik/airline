2b7950a390d4376ffe4aac12048473e8b4d3b226
saikamaik <saikamaik111@gmail.com>
2026-02-20 10:34:34 +0300

    Fix Swagger UI: use relative server URL to work on Railway

--- airline/src/main/java/com/example/airline/config/OpenApiConfig.java
+++ airline/src/main/java/com/example/airline/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +17,7 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(new Server().url("/").description("Current server")))
                 .info(new Info()
                         .title("Airline Tour Agency API")
                         .version("1.0")
